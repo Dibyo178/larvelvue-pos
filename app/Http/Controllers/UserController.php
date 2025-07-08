f9abd27d9e0dfc2e88ae36edfcee2e3e33e4ce07
Dibyo178 <Sourov.mu@gmail.com>
2025-07-08 23:57:14 +0600

    migrations-done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,13 +23,15 @@ class UserController extends Controller
                 'name' => 'required',
                 'email' => 'required|email:unique:users,email',
                 'password' => 'required',
+                'mobile'=>'required'
             ]);
 
             $user = User::create([
 
                 'name' => $request->input('name'),
                 'email' => $request->input('email'),
-                'password' => $request->input('password')
+                'password' => $request->input('password'),
+                'mobile'=>$request->input('mobile')
 
             ]);
 
